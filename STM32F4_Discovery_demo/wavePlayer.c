6b39f4a45ca74211775e4a5340258a7349aa9a3f
kebno <jboyle@cecs.pdx.edu>
2013-06-01 17:22:36 -0700

    Remove code that won't go into beamformer.

--- STM32F4_Discovery_demo/wavePlayer.c
+++ STM32F4_Discovery_demo/wavePlayer.c
@@ -8,22 +8,14 @@
 #include "wavePlayer.h"
 
 static WORKING_AREA(waThread3, 1024);
-
 #define PLAYBACK_BUFFER_SIZE	512
-
 uint8_t buf[PLAYBACK_BUFFER_SIZE]={0};
 uint8_t buf2[PLAYBACK_BUFFER_SIZE]={0};
-
 uint32_t waveSampleLength=0, bytesToPlay;
-
 Thread* playerThread;
-
 FIL f1;
-
 extern stm32_dma_stream_t* i2sdma;
-
 uint16_t vol=200;
-
 uint8_t pause=0;
 
 static void wavePlayEventHandler(uint8_t evt)
@@ -43,25 +35,25 @@ static void wavePlayEventHandler(uint8_t evt)
 			ui_displayPreviousMenu();
 			break;
 
-		case BTN_LEFT:
+		case BTN_LEFT:  // END PLAYBACK
 			chThdTerminate(playerThread);
 			chThdWait(playerThread);
 			playerThread=NULL;
 			break;
 
-		case BTN_MID_DOWN:
+		case BTN_MID_DOWN:  // VOLUME UP
 			vol--;
 			if (vol < 150) vol=150;
 			codec_volCtl(vol);
 			break;
 
-		case BTN_MID_UP:
+		case BTN_MID_UP:    // VOLUME DOWN
 			vol++;
 			if (vol > 220) vol=220;
 			codec_volCtl(vol);
 			break;
 
-		case BTN_MID_SEL:
+		case BTN_MID_SEL:   // PAUSE PLAYBACK
 			if (!pause) {
 				pause=1;
 				ui_drawBottomBar("Stop", "Play", "Exit");
@@ -74,14 +66,14 @@ static void wavePlayEventHandler(uint8_t evt)
 	}
 }
 
-static msg_t wavePlayerThread(void *arg) {
+static msg_t wavePlayerThread(void *arg) {  // THE PLAYER THREAD
 	(void)arg;
-	chRegSetThreadName("blinker");
+	chRegSetThreadName("PLAYER");
 
 	UINT temp, bufSwitch=1;
 
-	codec_pwrCtl(1);
-	codec_muteCtl(0);
+	codec_pwrCtl(1);    // POWER ON
+	codec_muteCtl(0);   // MUTE OFF
 
 	f_read(&f1, buf, PLAYBACK_BUFFER_SIZE, &temp);
 	bytesToPlay-=temp;
@@ -124,7 +116,7 @@ static msg_t wavePlayerThread(void *arg) {
 	return 0;
 }
 
-void playWaveFile(void)
+void playWaveFile(void)  // Initializes the Thread
 {
 
 	uint8_t st;
@@ -133,112 +125,13 @@ void playWaveFile(void)
 
 	uint32_t temp, offset=44, cnt;
 
-	ui_clearUserArea();
-	ui_drawTitleBar("Play Wave File");
-	ui_drawBottomBar("Stop", pause?"Play":"Pause", "Exit");
 	ui_sethandler((pfn)wavePlayEventHandler);
-
-	cx=3; cy=UI_TBARHEIGHT+3;
-
-	if (playerThread) {
-		lcd_puts("File Already Playing!");
-		return;
-	}
-
-	st=f_open(&f1, "audio.wav", FA_READ);
-
-	if (st) {
-		lcd_puts("Invalid file");
-		return;
-	}
 
 	st=f_read(&f1, buf, 1024, &btr);
 
 	wf=(WAVFILE*)&buf[0];
 
-	if (wf->riffSignature != WAVE_HEAD_RIFF || wf->fmtSignature != WAVE_HEAD_FMT || wf->format!=0x01) {
-		lcd_puts("This is a not a wave file!");
-		return;
-	}
-
-	xprintf("Number of channels=%d\nSample Rate=%ld\nNumber of Bits=%d\n", wf->numChannels, wf->sampleRate, wf->numBits);
-
-	temp=READ_UINT32(&buf[36]);
-
-	if (temp==WAVE_HEAD_DATA)
-	{
-		// Got 'data' chunk, ready to play
-		waveSampleLength=READ_UINT32(&buf[40]);
-		lcd_puts("No Metadata. Ready to play.\n");
-		offset=44;
-	}
-	else if (temp==WAVE_META_LIST)
-	{
-		uint32_t msize;
-		lcd_puts("File has metadata:");
-		msize=READ_UINT32(&buf[40]);
-		offset=52+msize;
-		xprintf("%ld bytes.\nActual Data starts at offset:%ld\n", msize, offset);
-
-		cnt=0;
-		temp=READ_UINT32(&buf[44+cnt]);
-
-		// Tag Reading Supported only on Audacity Output
-		if (temp==WAVE_META_INFO)
-		{
-			cnt+=4;
-			do
-			{
-				temp=READ_UINT32(&buf[44+cnt]);
-
-				switch (temp)
-				{
-					case WAVE_META_INAM:
-						cnt+=4;
-						temp=READ_UINT32(&buf[44+cnt]);
-						cnt+=4;
-						xprintf("Name: %s\n", &buf[44+cnt]);
-						cnt+=temp;
-						break;
-
-					case WAVE_META_IART:
-						cnt+=4;
-						temp=READ_UINT32(&buf[44+cnt]);
-						cnt+=4;
-						xprintf("Artist: %s\n", &buf[44+cnt]);
-						cnt+=temp;
-						break;
-
-					case WAVE_META_ICMT:
-						cnt+=4;
-						temp=READ_UINT32(&buf[44+cnt]);
-						cnt+=4;
-						xprintf("Composer: %s\n", &buf[44+cnt]);
-						cnt+=temp;
-						break;
-
-					case WAVE_META_ICRD:
-						cnt+=4;
-						temp=READ_UINT32(&buf[44+cnt]);
-						cnt+=4;
-						xprintf("Release Yr: %s\n", &buf[44+cnt]);
-						cnt+=temp;
-						break;
-
-					default:
-						cnt+=8;			// Token+Size (4 bytes each)
-						break;
-				}
-
-			} while (cnt < msize);
-			waveSampleLength=READ_UINT32(&buf[offset-4]);
-			lcd_puts("Ready To Play.\n");
-		}
-	}
-
 	codec_i2s_init(wf->sampleRate, wf->numBits);
-
-	xprintf("Sample Length:%ld bytes", waveSampleLength);
 
 	bytesToPlay=waveSampleLength;
 
--- STM32F4_Discovery_demo/wavePlayer.h
+++ STM32F4_Discovery_demo/wavePlayer.h
@@ -10,29 +10,11 @@
 
 #include "ch.h"
 #include "hal.h"
-
-#include "UI.h"
-#include "ff.h"
-
 #include "codec.h"
-
 #include "chprintf.h"
-#include "xprintf.h"
 
 #define READ_UINT32(ptr)		*((uint32_t*)ptr)
 #define READ_UINT16(ptr)		*((uint16_t*)ptr)
-
-#define WAVE_HEAD_RIFF			0x46464952		// 'FIRR' = RIFF in Little-Endian format
-#define WAVE_HEAD_WAVE			0x45564157		// WAVE in Little-Endian
-#define WAVE_HEAD_DATA			0x61746164		// 'data' in Little-Endian
-#define WAVE_HEAD_FMT			0x20746D66
-
-#define WAVE_META_LIST			0x5453494C
-#define WAVE_META_INFO			0x4F464E49
-#define WAVE_META_INAM			0x4D414E49
-#define WAVE_META_IART			0x54524149
-#define WAVE_META_ICMT			0x544D4349
-#define WAVE_META_ICRD			0x44524349
 
 typedef struct _wavfile {
 	uint32_t riffSignature;
