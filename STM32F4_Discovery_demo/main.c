c626984b53eb8605bf0ba32bc4a41303075a2b74
Piers Titus van der Torren <pierstitus@gmail.com>
2014-03-14 00:19:16 +0100

    Removes code not necessary for audio, add beep
    Compiles, but doesn't make sound

--- STM32F4_Discovery_demo/main.c
+++ STM32F4_Discovery_demo/main.c
@@ -4,29 +4,37 @@
 #include "hal.h"
 
 #include "chprintf.h"
-#include "ff.h"
 
 // User Includes
-#include "wavePlayer.h"
+#include "codec.h"
 
-void mainEventHandler(uint8_t evt);
-
-void playWaveFile(void);
+Thread* playerThread;
 
 // Application Title
-const char appTitle[]="STM32F4D-ChibiOS LCD GUI Demo";
+const char appTitle[]="STM32F4D-ChibiOS Audio Demo";
 Thread* mainThread;
 
+/*
+ * This is a periodic thread that does absolutely nothing except flashing
+ * a LED.
+ */
+static WORKING_AREA(waThread1, 128);
 __attribute__  ((noreturn))
 static msg_t Thread1(void *arg) {
 	(void)arg;
 	chRegSetThreadName("blinker");
 
+	codec_i2s_init(44100, 16);
+
+	codec_pwrCtl(1);    // POWER ON
+	codec_muteCtl(0);   // MUTE OFF
 	while (TRUE)
 	{
-		key=getkey();
-		ui_executecmd(key);
-		chThdSleep(50);
+		palSetPad(GPIOD, GPIOD_LED3);       /* Orange.  */
+		chThdSleepMilliseconds(500);
+		palClearPad(GPIOD, GPIOD_LED3);     /* Orange.  */
+		chThdSleepMilliseconds(500);
+		codec_sendBeep();
 	}
 }
 
@@ -45,8 +53,8 @@ static VirtualTimer vt1;
 
 static void led_toggle(void *p)
 {
-	p;
-	palTogglePad(GPIOD, GPIOD_LED3);
+	(void)p;
+	palTogglePad(GPIOD, GPIOD_LED4);
 	chVTSetI(&vt1, 500, led_toggle, NULL);
 }
 
@@ -60,12 +68,12 @@ int main(void)
 
 	hw_init();
 
-	chThdCreateFromHeap(NULL, 1024, NORMALPRIO, Thread1, NULL);
+	//chThdCreateFromHeap(NULL, 1024, NORMALPRIO, Thread1, NULL);
+	playerThread=chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
 
 	chVTSetI(&vt1, 500, led_toggle, NULL);
 
 	while (TRUE) {
-		ui_executecmd(0xFF);
 		chThdSleep(100);
 	}
 }
